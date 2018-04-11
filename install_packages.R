9a24f9fa9ea4fc8203703e5e643c87e0b7502d25
Gergely Szilagyi <esziger@gmail.com>
2018-04-11 15:57:04 +0200

    Add intall_packages script
    Run install_packages script
    before running Script.rmd

---
+++ install_packages.R
@@ -0,0 +1,14 @@
+install.packages("ggthemes")
+install.packages("ggridges")
+install.packages("ggforce")
+install.packages("ggExtra")
+install.packages("GGally")
+install.packages("gridExtra")
+install.packages("corrplot")
+install.packages("VIM")
+install.packages("xgboost")
+install.packages("randomForest")
+install.packages("ROCR")
+
+
+
